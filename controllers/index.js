1604fb8653cdd7ee35c19e386e75c9ea3e225f38
fakhryhizballah <falehry88@gmail.com>
2023-09-08 14:19:09 +0700

    endpoint poli

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,32 @@
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const axios = require('axios');
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.JWT_SECRET_KEY;
+const payload = {
+  gid: "Server Side",
+};
 
+async function api(path, method, data) {
+  let token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  try {
+    let config = {
+      method: method,
+      url: process.env.HOST_API + path,
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      data: data
+    };
+    let response = await axios(config);
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    return null;
+  }
+}
 
 module.exports = {
   hakpasien: async (req, res) => {
@@ -24,4 +49,40 @@ module.exports = {
       });
     }
   },
+  getPoli: async (req, res) => {
+    try {
+      let data = await api('/api/ralan/poli', 'GET');
+      console.log(data);
+      return res.status(200).json({
+        status: true,
+        message: "success",
+        data: data.data
+      });
+    } catch (err) {
+      return res.status(500).json({
+        status: false,
+        message: "server Error",
+        data: err.message
+      });
+    }
+  },
+  getJadwalPoli: async (req, res) => {
+    try {
+      let query = req.query;
+      let data = await api('/api/ralan/jadwal?kd_poli=' + query.kd_poli, 'GET');
+      console.log(data);
+      return res.status(200).json({
+        status: true,
+        message: "success",
+        data: data.data
+      });
+    } catch (err) {
+      return res.status(500).json({
+        status: false,
+        message: "server Error",
+        data: err.message
+      });
+    }
+  },
+
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 const base = require('../controllers');
 
 routes.get('/umum/:id', base.hakpasien);
+routes.get('/poli/all', base.getPoli);
+routes.get('/poli/jadwal', base.getJadwalPoli);
 
 
 module.exports = routes;
